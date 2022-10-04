de61dd41118963e4589eb3fc9a0e2c46a23fadc1
nmwangala <mwangalanelson@gmail.com>
2022-10-04 16:20:20 +0300

    Added section-1 basic customer api

--- src/main/java/com/springboot/springbootmasterclass/SpringbootmasterclassApplication.java
+++ src/main/java/com/springboot/springbootmasterclass/SpringbootmasterclassApplication.java
@@ -2,12 +2,45 @@ package com.springboot.springbootmasterclass;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class SpringbootmasterclassApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootmasterclassApplication.class, args);
 	}
+	
+	@GetMapping
+	Customer getCustomer() {
+		return new Customer(1L,"Nelson Mwangala");
+	}
 
+	
+	class Customer{
+		
+		private final Long id;
+		private final String name;
+		
+		
+		public Customer(Long id, String name) {
+			
+			this.id = id;
+			this.name = name;
+		}
+
+
+		public Long getId() {
+			return id;
+		}
+
+
+		public String getName() {
+			return name;
+		}
+		
+		
+	}
 }
