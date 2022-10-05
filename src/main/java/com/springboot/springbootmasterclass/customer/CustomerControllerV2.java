2322fd1dab04ca582415471cf855d9becbbc0cd0
nmwangala <mwangalanelson@gmail.com>
2022-10-05 09:38:30 +0300

    Added versioning

--- src/main/java/com/springboot/springbootmasterclass/customer/CustomerControllerV2.java
+++ src/main/java/com/springboot/springbootmasterclass/customer/CustomerControllerV2.java
@@ -13,21 +13,27 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("api/v1/customer")
-@Deprecated
-public class CustomerController {
+@RequestMapping("api/v2/customer")
+
+public class CustomerControllerV2 {
 
 	private final CustomerService customerService;
 
 	@Autowired
-	public CustomerController(CustomerService customerService) {
+	public CustomerControllerV2(CustomerService customerService) {
 
 		this.customerService = customerService;
 	}
 
 	@GetMapping(value = "all")
-	List<Customer> getCustomer() {
+	List<Customer> getCustomers() {
 		return customerService.getCustomer();
+	}
+
+	@GetMapping(path="{customerId}")
+	Customer getCustomer(@PathVariable("customerId") Long id) {
+		return customerService.getCustomer().stream().filter(customer -> customer.getId().equals(id)).findFirst()
+				.orElseThrow(() -> new IllegalStateException("Customer not found"));
 	}
 
 	@PostMapping
@@ -37,6 +43,7 @@ public class CustomerController {
 		System.out.println(customer);
 
 	}
+
 	@PutMapping
 	void updateCustomer(@RequestBody Customer customer) {
 
