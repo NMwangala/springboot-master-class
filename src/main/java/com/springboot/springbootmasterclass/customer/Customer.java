60d4b7f64629332425aabcfcf68c26c140ac7fc9
nmwangala <mwangalanelson@gmail.com>
2022-10-07 18:33:41 +0300

    Added validations

--- src/main/java/com/springboot/springbootmasterclass/customer/Customer.java
+++ src/main/java/com/springboot/springbootmasterclass/customer/Customer.java
@@ -1,20 +1,30 @@
 package com.springboot.springbootmasterclass.customer;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonProperty.Access;
 
 public class Customer {
 	private final Long id;
+	@NotBlank
 	private final String name;
+	@NotBlank
 	@JsonProperty(access = Access.WRITE_ONLY)
 	private final String password;
+	
+	@NotBlank
+	@Email
+	private String email;
 
-	public Customer(Long id, String name, String password) {
+	public Customer(Long id, String name, String password,String email) {
 
 		this.id = id;
 		this.name = name;
 		this.password = password;
+		this.email=email;
 	}
 
 	@JsonProperty("customer_id")
@@ -30,10 +40,16 @@ public class Customer {
 	public String getPassword() {
 		return password;
 	}
+	
+	public String getEmail() {
+		return email;
+	}
 
 	@Override
 	public String toString() {
-		return "Customer [id=" + id + ", name=" + name + ", password=" + password + "]";
+		return "Customer [id=" + id + ", name=" + name + ", password=" + password + ", email=" + email + "]";
 	}
+
+	
 
 }
--- src/main/java/com/springboot/springbootmasterclass/customer/CustomerFakeRepository.java
+++ src/main/java/com/springboot/springbootmasterclass/customer/CustomerFakeRepository.java
@@ -12,8 +12,8 @@ public class CustomerFakeRepository implements CustomerRepo{
 	@Override
 	public List<Customer> getCustomers() {
 		return Arrays.asList(
-			new Customer(1L,"Nelson Mwangala","Password123"),
-			new Customer(2L,"Nillan Barawa","123Password")
+			new Customer(1L,"Nelson Mwangala","Password123","email@gmail.com"),
+			new Customer(2L,"Nillan Barawa","123Password","email@gmail.com")
 			);
 		}
 	
