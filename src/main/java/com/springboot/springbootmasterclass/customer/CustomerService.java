fecb4dc105e6f6ff6a484cda4d7deba1bdffae2f
nmwangala <mwangalanelson@gmail.com>
2022-10-11 17:40:04 +0300

    Added logger

--- src/main/java/com/springboot/springbootmasterclass/customer/CustomerService.java
+++ src/main/java/com/springboot/springbootmasterclass/customer/CustomerService.java
@@ -2,6 +2,8 @@ package com.springboot.springbootmasterclass.customer;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
@@ -10,6 +12,8 @@ import com.springboot.springbootmasterclass.exception.NotFoundException;
 
 @Service
 public class CustomerService {
+	
+	private final static Logger LOGGER = LoggerFactory.getLogger(CustomerService.class);
 
 	private final CustomerRepository  customerRepository;
 	
@@ -21,11 +25,17 @@ public class CustomerService {
 
 
 	List<Customer> getCustomers() {
+		LOGGER.info("Get customer was called");
 		return customerRepository.findAll();
 	}
 	
 	Customer getCustomer(Long id) {
-		return customerRepository.findById(id).orElseThrow(() -> new NotFoundException("Customer with ID " +id +" not found"));
+		return customerRepository.findById(id).orElseThrow(() ->{
+			NotFoundException notFoundException=	new NotFoundException("Customer with ID " +id +" not found");
+			
+			LOGGER.error("Error in getting customer {}",id, notFoundException.toString());
+			return notFoundException;
+		});
 //		return getCustomers().stream().filter(customer -> customer.getId().equals(id)).findFirst()
 //				.orElseThrow(() -> new NotFoundException("Customer with ID " +id +" not found"));
 	}
