95ed56f273878cf009e6729f3908883b48602cdb
nmwangala <mwangalanelson@gmail.com>
2022-10-12 09:13:52 +0300

    Implemented configuration properties

--- src/main/java/com/springboot/springbootmasterclass/customer/CustomerConfiguration.java
+++ src/main/java/com/springboot/springbootmasterclass/customer/CustomerConfiguration.java
@@ -1,8 +1,10 @@
 package com.springboot.springbootmasterclass.customer;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -10,10 +12,17 @@ public class CustomerConfiguration {
 
 	@Value("${app.useFakeCustomerRepo:false}")
 	private Boolean useFakeCustomerRepo;
+	
+	@Value("${info.company.name}")
+	private String companyName;
+	
+	@Autowired
+	private Environment environment;
 	@Bean
 	CommandLineRunner commandLineRunner() {
 		return args ->{
 			System.out.println("CommandLine runner, Hurray");
+			System.out.println(environment.getProperty("info.company.name"));
 		};
 	}
 	
